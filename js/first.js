9d7c4af4510d8e6348ef9209c15f4dafc89e60f4
Sanket Tantia <22017124+Sanket-Tantia@users.noreply.github.com>
2018-02-08 19:11:03 +0530

    Added announcement page to controller
    Refactored code

--- js/first.js
+++ js/first.js
@@ -1,94 +1,4 @@
 
-
-/*var app = angular.module('rmsApp', ['ngRoute']);
-
-
-app.config(function($routeProvider) {
-
-	$routeProvider.when('/announcements', {
-		templateUrl : '/js/announcements/announcements.html',
-		controller : 'announcementController',
-		/*resolve: {
-       	 usersResource: function($rootScope) {
-       		if ($rootScope.resource)
-        		 return $rootScope.resource.$get('users', {
-        	            linksAttribute: "_links",
-        	            embeddedAttribute: "_embedded"
-        	        });
-       		else
-       			 return {};
-       	 	}
-        }
-	})
-	.when('/employeeSearch', {
-    		templateUrl : '/js/employeeSearch/employeeSearch.html',
-    		/*controller : 'announcementController',
-    		resolve: {
-           	 usersResource: function($rootScope) {
-           		if ($rootScope.resource)
-            		 return $rootScope.resource.$get('users', {
-            	            linksAttribute: "_links",
-            	            embeddedAttribute: "_embedded"
-            	        });
-           		else
-           			 return {};
-           	 	}
-            }
-    	})
-    	.when('/sendEmailForm', {
-        		templateUrl : '/js/sendEmailForm/sendEmailForm.html',
-        		/*controller : 'announcementController',
-        		resolve: {
-               	 usersResource: function($rootScope) {
-               		if ($rootScope.resource)
-                		 return $rootScope.resource.$get('users', {
-                	            linksAttribute: "_links",
-                	            embeddedAttribute: "_embedded"
-                	        });
-               		else
-               			 return {};
-               	 	}
-                }
-      })
-      .when('/uploadAnnouncements', {
-                    		templateUrl : '/js/uploadAnnouncements/uploadAnnouncements.html',
-                    		/*controller : 'announcementController',
-                    		resolve: {
-                           	 usersResource: function($rootScope) {
-                           		if ($rootScope.resource)
-                            		 return $rootScope.resource.$get('users', {
-                            	            linksAttribute: "_links",
-                            	            embeddedAttribute: "_embedded"
-                            	        });
-                           		else
-                           			 return {};
-                           	 	}
-                            }
-                    	})
-.when('/operations', {
-        		templateUrl : 'operations.html',
-        		controller : 'OpController',
-        		/*resolve: {
-               	 usersResource: function($rootScope) {
-               		if ($rootScope.resource)
-                		 return $rootScope.resource.$get('users', {
-                	            linksAttribute: "_links",
-                	            embeddedAttribute: "_embedded"
-                	        });
-               		else
-               			 return {};
-               	 	}
-                }
-        	})
-        	.when('/login', {
-        		templateUrl : '/js/login/login.html',
-        		controller : 'loginController'
-        	})
-          .otherwise({
-          		redirectTo : '/operations'
-          	});
-  });*/
-
 var app = angular.module('rmsApp',['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider){
@@ -99,10 +9,10 @@ app.config(function($stateProvider, $urlRouterProvider){
     templateUrl: 'operations.html',
     controller: 'OpController'
   })
-  .state('signup',{
-    url: '/signup',
-    templateUrl: 'signup.html',
-    controller: 'SignupCtrl'
+  .state('announcement',{
+    url: '/announcement',
+    templateUrl: 'announcements.html',
+    controller: 'AnCtrl'
   })
   $urlRouterProvider.otherwise('/operations');
 });
